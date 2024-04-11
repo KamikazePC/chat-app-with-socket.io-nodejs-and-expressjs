dba18b6f2c556171da3eabe152fd4b80b9a3c183
PriestlyC <145232298+KamikazePC@users.noreply.github.com>
2024-04-11 17:37:39 +0100

    Update app.js

--- Server/public/app.js
+++ Server/public/app.js
@@ -56,7 +56,7 @@ socket.on("message", (data) => {
     const li = document.createElement('li')
     li.className = 'post'
     if (name === nameInput.value) li.className = 'post post--left'
-    if (name !== nameInput.value && name!== 'Admin') li.className = 'post post--right'
+    if (name !== nameInput.value && name !== 'Admin') li.className = 'post post--right'
     if (name !== 'Admin') {
         li.innerHTML = `<div class ="post__header ${name ===
             nameInput.value
